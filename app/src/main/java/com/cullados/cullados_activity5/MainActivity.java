ddeda7fdca157f87595633f41ed34826fa46b620
Dexter Cullados <culladosjohndexter@gmail.com>
2026-03-05 23:59:46 +0800

    finished layout

--- app/src/main/java/com/cullados/cullados_activity5/MainActivity.java
+++ app/src/main/java/com/cullados/cullados_activity5/MainActivity.java
@@ -12,13 +12,27 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //declare variables here
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //connect java var to xml id here
+
+        //add button action here
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-}
+
+
+    //put method here
+
+
+} //class ending
